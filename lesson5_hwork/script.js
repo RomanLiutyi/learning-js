6aaa71619d0b7953bc6cb2c22b88f39329ca93d2
Rom4ik <you@example.com>
2019-06-07 13:38:53 +0300

    lucky number with EXIT & protect from idiot

---
+++ lesson5_hwork/script.js
@@ -0,0 +1,18 @@
+// var guess,
+//     rand = Math.round(Math.random()*5),
+//     i   = 0;
+// do {
+//     guess = prompt('Введите ваш ответ ');
+//     i++ ;
+
+//     if ( typeof(guess) === Object){
+//         break;
+//     }else if ( isNaN(guess)){
+//         console.log('Вы ввели строку вместо числа');
+//         break;
+//     }else if ( +guess === rand) {
+//         console.log('Поздравляем', rand, '-загаданое число');
+//         console.log('Вам потребовалось ', i ,' попытки');
+//     }
+// } while ( guess!==null && +guess !== rand);
+
